8b25a2310616312a7e70e47cadd361510af33782
Gideon Gbadebo <oluwaayanfe298@gmail.com>
2026-09-21 17:14:52 +0100

    Update register.py

--- register.py
+++ register.py
@@ -5,7 +5,7 @@ Renders the registration screen and creates new user accounts.
 """
 
 import streamlit as st
-from utils import authentication as auth
+import authentication as auth
 
 
 def render():
